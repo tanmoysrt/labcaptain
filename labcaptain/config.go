12ce0c80c9eb6349b6c586cc5c557b1828d4a799
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-09-17 01:44:02 +0530

    feat: prometheus config update added

--- labcaptain/config.go
+++ labcaptain/config.go
@@ -24,28 +24,22 @@ scrape_configs:
 #start_targets_list
 #end_targets_list
 `
-	// if /etc/prometheus/prometheus.yml exist, read the file
-	if _, err := os.Stat("/etc/prometheus/prometheus.yml"); err == nil {
-		configContentBytes, err := os.ReadFile("/etc/prometheus/prometheus.yml")
-		if err != nil {
-			return "", errors.New("Failed to read /etc/prometheus/prometheus.yml > " + err.Error())
-		}
-		configContent = string(configContentBytes)
-	}
-
 	// Ensure that the tags exist in the config
 	startTag := "#start_targets_list"
 	endTag := "#end_targets_list"
 
-	if !strings.Contains(configContent, startTag) || !strings.Contains(configContent, endTag) {
-		errMsg := "Configuration file is missing target list tags.\n"
-		errMsg += "Please add the following tags to insert the targets list:\n"
-		errMsg += fmt.Sprintf("\t%s\n", startTag)
-		errMsg += fmt.Sprintf("\n%s", endTag)
-		return "", errors.New(errMsg)
+	// if /etc/prometheus/prometheus.yml exist, read the file
+	if _, err := os.Stat("/etc/prometheus/prometheus.yml"); err == nil {
+		existingConfigContentBytes, err := os.ReadFile("/etc/prometheus/prometheus.yml")
+		if err != nil {
+			return "", errors.New("Failed to read /etc/prometheus/prometheus.yml > " + err.Error())
+		}
+		existingConfigContent := string(existingConfigContentBytes)
+		if strings.Contains(existingConfigContent, "#start_targets_list") && strings.Contains(existingConfigContent, "#end_targets_list") {
+			configContent = existingConfigContent
+		}
 	}
 
-	// Get the l
 	servers, err := GetAllServers()
 	if err != nil {
 		return "", err
